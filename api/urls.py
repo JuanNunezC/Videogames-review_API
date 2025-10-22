5adfe0ad4bffc1cd4e35831dc558e62ae17629a6
Juan Nuñez <juancarchenemy@gmail.com>
2025-10-22 15:23:39 -0700

    Implementation of Game Data obtained through Rate Game Page

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import random_number, get_igdb_access_token_view, search_games
+from .views import random_number, get_igdb_access_token_view, search_games, get_game_by_id
 
 urlpatterns = [
     path("random/", random_number),
     path("igdb-token/", get_igdb_access_token_view),
     path("search-games/", search_games),
+    path("game/<int:id>/", get_game_by_id), 
 ]
