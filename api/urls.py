21d6abc386ce181c365ad50844ad2761e48f569d
Juan Nuñez <juancarchenemy@gmail.com>
2025-11-06 12:14:27 -0700

    Firebase and login implementation

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import random_number, get_igdb_access_token_view, search_games, get_game_by_id
-
+from .views import( random_number, get_igdb_access_token_view, search_games, get_game_by_id 
+,csrf_token,create_session,logout,get_profile
+)
 urlpatterns = [
     path("random/", random_number),
     path("igdb-token/", get_igdb_access_token_view),
     path("search-games/", search_games),
     path("game/<int:id>/", get_game_by_id), 
+
+    #Auth (Firebase)
+    path("auth/session/", create_session),
+    path("auth/logout/", logout),
+    path("profile/", get_profile),
+    path("auth/csrf/", csrf_token),
+
 ]
